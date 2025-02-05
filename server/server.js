11d6e42fcefa2c5856c41b9f429bcfcce64f243d
Ayumi <ayuayuayuayus@gmail.com>
2025-02-05 10:25:01 +0100

    CHANGE port 3000 into 8080 on server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
